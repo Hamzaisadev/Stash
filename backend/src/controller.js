b0e249ad603578018bd7889abad1544e153cd99d
Hamza Ishaq <ishaqhamza212@gmail.com>
2026-07-09 22:03:12 +0500

    feat: implement file upload, retrieval, and download controllers with password protection and expiration management

--- backend/src/controller.js
+++ backend/src/controller.js
@@ -135,3 +135,78 @@ export const getFilees = async (req, res, next) => {
 }
 
 // download controller
+
+export const downloadFile = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { password } = req.body;
+
+        await cleanupExpiredFiles()
+        const file = await dbGet('SELECT * FROM files WHERE id = ?', [id]);
+
+        if (!file) {
+            return next(new AppError('File not found or has expired', 404));
+        }
+
+        if (file.max_downloads && file.download_count >= file.max_downloads) {
+            if (fs.existsSync(file.file_path)) {
+                fs.unlinkSync(file.file_path)
+            }
+
+            await dbRun('DELETE FROM files WHERE id = ?', [file.id]);
+            return next(new AppError('This file has reached its download limit and is no longer available.', 410));
+
+        }
+
+        if (file.password_hash) {
+            if (!password) {
+                return next(new AppError('This file is password protected. Please provide a password', 401));
+
+            }
+
+            const isMatch = bcrypt.compareSync(password, file.password_hash)
+            if (!isMatch) {
+                return next(new AppError('Incorrect password. Acess denied', 403))
+
+            }
+        }
+        if (!fs.existsSync(file.file_path)) {
+            return next(new AppError('Physical file missing from server storage', 404))
+        }
+
+        const newDownloadCount = file.download_count + 1;
+
+        await dbRun(`UPDATE files SET download_count = ? WHERE id = ?`, [newDownloadCount, file.id])
+
+        res.setHeader('Content-Disposition', `attachment;
+            filename= "${encodeURIComponent(file.filename)}"`
+        )
+        res.setHeader('Content-type', file.mime_type);
+        res.setHeader('Content-length', file.file_size);
+
+
+        const fileStream = fs.createReadStream(file.file_path);
+        fileStream.pipe(res);
+
+        fileStream.on('end', async () => {
+            if (file.max_downloads && newDownloadCount >= file.max_downloads) {
+                try {
+                    if (fs.existsSync(file.file_path)) {
+
+                        fs.unlinkSync(file.file_path)
+                    }
+                    await dbRun('DELETE FROM files WHERE id = ?', [file.id])
+                    console.log(`File ${file.filename} was successfully 'burned' after reaching limit.`)
+                } catch (err) {
+                    console.error('Error burning file:', err)
+                }
+            }
+        })
+        fileStream.on('error', (err) => {
+            console.error('Error streaming file:', err)
+        })
+    } catch (err) {
+        next(err)
+    }
+
+}
